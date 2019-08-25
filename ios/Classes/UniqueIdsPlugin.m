6264bd30328740ba2a15c7be24013578afb315f9
Aleksey Goncharov <x401om@gmail.com>
2019-08-25 13:43:35 +0300

    iOS Plugin Converted to Objective-C due to build errors

--- ios/Classes/UniqueIdsPlugin.m
+++ ios/Classes/UniqueIdsPlugin.m
@@ -1,8 +1,29 @@
 #import "UniqueIdsPlugin.h"
-#import <unique_ids/unique_ids-Swift.h>
+#import <AdSupport/ASIdentifierManager.h>
 
 @implementation UniqueIdsPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftUniqueIdsPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"unique_ids"
+                                     binaryMessenger:[registrar messenger]];
+    UniqueIdsPlugin* instance = [[UniqueIdsPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([call.method isEqualToString:@"adId"]) {
+        NSUUID *adid = [[ASIdentifierManager sharedManager] advertisingIdentifier];
+        result(adid.UUIDString);
+    } else if ([call.method isEqualToString:@"uuid"]) {
+        result([NSUUID UUID].UUIDString);
+    }
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
+
 @end
